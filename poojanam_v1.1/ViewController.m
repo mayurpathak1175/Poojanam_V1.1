034dc6e334b71622c868fb7cf9fe418f076d125c
Mayur Pathak <mayurpathak@macbook-pro.local>
2016-07-26 21:38:09 -0700

    Initial Commit

---
+++ poojanam_v1.1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  poojanam_v1.1
+//
+//  Created by Mayur Pathak on 7/26/16.
+//  Copyright © 2016 Mayur Pathak. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ poojanam_v1.1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  poojanam_v1.1
+//
+//  Created by Mayur Pathak on 7/26/16.
+//  Copyright © 2016 Mayur Pathak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
